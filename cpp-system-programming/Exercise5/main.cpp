58cd4a180a43348af0c8a7870c05478a0c85cc45
DV1X3R <d.purvinsh@gmail.com>
2018-04-18 14:55:23 +0300

    Output fixed according to the task

--- cpp-system-programming/Exercise5/main.cpp
+++ cpp-system-programming/Exercise5/main.cpp
@@ -7,6 +7,7 @@ using namespace std;
 
 bool RegSearch(HKEY hRootKey, TCHAR *cPath, bool bSearch = false, TCHAR *sSearchKeyName = NULL)
 {
+	_tprintf(_T(" %s\n"), cPath);
 	HKEY hKey;
 
 	// Открытие ключа
@@ -49,6 +50,7 @@ bool RegSearch(HKEY hRootKey, TCHAR *cPath, bool bSearch = false, TCHAR *sSearch
 
 					if (dwRegEnumValue == ERROR_SUCCESS) {
 						DWORD dwType;
+						TCHAR *cType = _T("");
 						DWORD dwValueBufferLen = dwMaxValueLen + 1;
 						BYTE *bValueBuffer = new BYTE[dwValueBufferLen];
 						ZeroMemory(bValueBuffer, sizeof(bValueBuffer));
@@ -58,29 +60,32 @@ bool RegSearch(HKEY hRootKey, TCHAR *cPath, bool bSearch = false, TCHAR *sSearch
 
 						if (dwRegQueryValueEx == ERROR_SUCCESS)
 						{
-							TCHAR *type = _T("");
-
 							switch (dwType)
 							{
-							case REG_BINARY: type = _T("REG_BINARY"); break;
-							case REG_DWORD: type = _T("REG_DWORD"); break;
-							case REG_DWORD_BIG_ENDIAN: type = _T("REG_DWORD_BIG_ENDIAN"); break;
-							case REG_EXPAND_SZ: type = _T("REG_EXPAND_SZ"); break;
-							case REG_LINK: type = _T("REG_LINK"); break;
-							case REG_MULTI_SZ: type = _T("REG_MULTI_SZ"); break;
-							case REG_NONE: type = _T("REG_NONE"); break;
-							case REG_QWORD: type = _T("REG_QWORD"); break;
-							case REG_SZ: type = _T("REG_SZ"); break;
+							case REG_BINARY: cType = _T("REG_BINARY"); break;
+							case REG_DWORD: cType = _T("REG_DWORD"); break;
+							case REG_DWORD_BIG_ENDIAN: cType = _T("REG_DWORD_BIG_ENDIAN"); break;
+							case REG_EXPAND_SZ: cType = _T("REG_EXPAND_SZ"); break;
+							case REG_LINK: cType = _T("REG_LINK"); break;
+							case REG_MULTI_SZ: cType = _T("REG_MULTI_SZ"); break;
+							case REG_NONE: cType = _T("REG_NONE"); break;
+							case REG_QWORD: cType = _T("REG_QWORD"); break;
+							case REG_SZ: cType = _T("REG_SZ"); break;
 							}
+						}
 
-							_tprintf(_T("\n%s\\%s \n \t Type: %s \n \t Data: %s\n"), cPath, cValueNameBuffer, type, bValueBuffer);
+						bool searchFinished = bSearch && !wcscmp(cValueNameBuffer, sSearchKeyName);
+
+						if (!bSearch || searchFinished) {
+							_tprintf(_T("\n%s\\%s \n \t Type: %s \n \t Data: %s\n"), cPath, cValueNameBuffer, cType, bValueBuffer);
 						}
 
 						// Если выбран поиск, и нужный ключ найден, то завершаем работу метода и завершаем обход в глубину
-						if (bSearch && !wcscmp(cValueNameBuffer, sSearchKeyName)) {
+						 if (searchFinished) {
 							RegCloseKey(hKey);
 							return true;
 						}
+						
 					}
 				}
 			}
@@ -106,12 +111,12 @@ bool RegSearch(HKEY hRootKey, TCHAR *cPath, bool bSearch = false, TCHAR *sSearch
 					if (dwRegEnumKeyEx == ERROR_SUCCESS) {
 						TCHAR *newPath = new TCHAR[MAX_PATH + 1];
 						ZeroMemory(newPath, sizeof(newPath));
-						
+
 						wcscat(newPath, cPath);
-						if(wcscmp(newPath, L"") != 0)
-							wcscat(newPath, L"\\"); 
+						if (wcscmp(newPath, L"") != 0)
+							wcscat(newPath, L"\\");
 						wcscat(newPath, cSubKeyBuffer);
-						
+
 						// Запуск рекурсии
 						if (RegSearch(hRootKey, newPath, bSearch, sSearchKeyName))
 							return true;
@@ -156,9 +161,9 @@ int main()
 	getline(cin, sKeyName);
 	wcscpy(cKeyName, wstring(sKeyName.begin(), sKeyName.end()).c_str());
 
-	if(sKeyName == "")
+	if (sKeyName == "")
 		RegSearch(hRootKey, cPath);
-	else 
+	else
 		RegSearch(hRootKey, cPath, true, cKeyName);
 
 	system("pause");
