3cc3ff6ad5c6e7dda4a0c768026704cad7fc9383
DV1X3R <d.purvinsh@gmail.com>
2018-05-16 22:24:49 +0300

    Exercise 8 added

---
+++ cpp-system-programming/Exercise8/main.cpp
@@ -0,0 +1,79 @@
+#include <windows.h>
+#include <iostream>
+
+struct MyStruct { char charVal; int intVal[500]; };
+
+int main()
+{
+	_SYSTEM_INFO si;
+	GetSystemInfo(&si);
+
+	printf("Page Size: %i \n", si.dwPageSize);
+	printf("Allocation Granularity: %i \n", si.dwAllocationGranularity);
+	printf("Number of pages available: %i \n", si.dwAllocationGranularity / si.dwPageSize);
+
+	LPVOID lpReservedMem = VirtualAlloc(
+		NULL // область для распределения или резервирования
+		, si.dwAllocationGranularity // размер области
+		, MEM_RESERVE // тип распределения (резервация области физической памяти)
+		, PAGE_READWRITE); // тип защиты доступа
+
+	LPVOID lpCommitedMem = VirtualAlloc(lpReservedMem, si.dwPageSize, MEM_COMMIT, PAGE_READWRITE); // Выполняется выделение страниц памяти для непосредственной работы с ними
+	DWORD pagesCommited = 1;
+	DWORD shift = 0;
+
+	printf("Virtual memory address: %p \n\n", lpCommitedMem);
+
+	int *inum = new int(1234);
+	double *dnum = new double(3.14);
+
+	int scount = 8;
+	MyStruct *structs = new MyStruct[scount];
+
+	for (int i = 0; i < scount; i++)
+	{
+		structs[i].intVal[499] = i;
+		structs[i].charVal = 'A' + i;
+	}
+
+	*(int*)lpCommitedMem = *inum;
+	printf("Integer added \t | %p \n", lpCommitedMem);
+	shift += sizeof(int);
+
+
+	*(double*)((char*)lpCommitedMem + shift) = *dnum;
+	printf("Double added \t | %p \n\n", ((char*)lpCommitedMem + shift));
+	shift += sizeof(double);
+
+	for (int i = 0; i < scount; i++)
+	{
+		__try
+		{
+			*(MyStruct*)((char*)lpCommitedMem + shift) = structs[i];
+			printf("Struct %i added | %p \n", i, ((char*)lpReservedMem + shift));
+			shift += sizeof(MyStruct);
+		}
+		__except (EXCEPTION_EXECUTE_HANDLER)
+		{
+			printf("\tException at %i structure \n", i);
+			VirtualAlloc(
+				((char*)lpReservedMem) + (si.dwPageSize * pagesCommited)
+				, si.dwPageSize, MEM_COMMIT, PAGE_READWRITE);
+			pagesCommited += 1;
+			i--;
+		}
+	}
+
+	printf("\n Pages used: ", pagesCommited);
+	printf(" %p : %i (integer) \n", lpReservedMem, *(int*)lpReservedMem);
+	printf(" %p : %f (double) \n", ((int*)lpReservedMem + 1), *(double*)((int*)lpReservedMem + 1));
+
+	for (int i = 0; i < scount; i++)
+	{
+		MyStruct *s = ((MyStruct*)((char*)lpCommitedMem + sizeof(int) + sizeof(double)) + i);
+		printf("%p | &intVal: %p | %i / %c \n", s, &s->intVal, s->intVal[499], s->charVal);
+	}
+
+	system("pause");
+	return 0;
+}
