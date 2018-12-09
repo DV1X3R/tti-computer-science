17a60b40932f66d5e081e512f5b6a0cc17428e1f
DV1X3R <d.purvinsh@gmail.com>
2018-12-10 01:12:18 +0200

    .NET: 'Exceptions' init

---
+++ IV. Fourth Year/net-dotnet-development/tsi-net-exceptions/ConsoleApp/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using ExceptionsLibrary;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Solve()
+        {
+            int radius = 0;
+            do { Console.Write("Please enter a cylinders radius> "); }
+            while (!int.TryParse(Console.ReadLine(), out radius));
+            if (radius < 0) { throw new NegativeRadiusException($"Incorrect radius value: {radius}"); }
+
+            int height = 0;
+            do { Console.Write("Please enter a cylinders height> "); }
+            while (!int.TryParse(Console.ReadLine(), out height));
+            if (height < 0) { throw new NegativeHeightException($"Incorrect height value: {height}"); }
+
+            Console.WriteLine($"S = 2PI * r * (h + r) = {2 * Math.PI * radius * (height + radius)}");
+        }
+
+        static void Main(string[] args)
+        {
+            try { Solve(); }
+            catch(NegativeRadiusException e)
+            {
+                Console.WriteLine("Negative Radius Exception");
+                Console.WriteLine($"Message: {e.Message}");
+                Console.WriteLine($"StackTrace: {e.StackTrace}");
+            }
+            catch(NegativeHeightException e)
+            {
+                Console.WriteLine("Negative Height Exception");
+                Console.WriteLine($"Message: {e.Message}");
+                Console.WriteLine($"StackTrace: {e.StackTrace}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ IV. Fourth Year/net-dotnet-development/tsi-net-exceptions/ExceptionsLibrary/Exceptions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace ExceptionsLibrary
+{
+    [Serializable]
+    public class NegativeRadiusException : ApplicationException
+    {
+        public NegativeRadiusException() { }
+        public NegativeRadiusException(string message) : base(message) { }
+        public NegativeRadiusException(string message, Exception ex) : base(message, ex) { }
+        protected NegativeRadiusException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+    }
+
+    [Serializable]
+    public class NegativeHeightException : ApplicationException
+    {
+        public NegativeHeightException() { }
+        public NegativeHeightException(string message) : base(message) { }
+        public NegativeHeightException(string message, Exception ex) : base(message, ex) { }
+        protected NegativeHeightException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+    }
+
+}
