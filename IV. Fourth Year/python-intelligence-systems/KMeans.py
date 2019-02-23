0a025a80e00298f1eaa26a6ef2f74e5a8c6c0141
DV1X3R <d.purvinsh@gmail.com>
2019-02-23 15:06:42 +0200

    IS: Exercise 1 done (RC1)

--- IV. Fourth Year/python-intelligence-systems/KMeans.py
+++ IV. Fourth Year/python-intelligence-systems/KMeans.py
@@ -4,7 +4,7 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 import imageio
-from sklearn.cluster import KMeans
+#from sklearn.cluster import KMeans
 
 
 class _KMeans:
@@ -21,87 +21,92 @@ class _KMeans:
     def fit(self, x):
         iter = 0
         self.x = np.array(x)
-        self._init_centroids()  # Step 0
-        while not np.array_equal(self.cluster_centers_old_, self.cluster_centers_) \
-                and iter < self.max_iter:
-            iter += 1
-            self.cluster_centers_old_ = self.cluster_centers_.copy()
-            self._calculate_labels()  # Step 1
-            self._calculate_centroids()  # Step 2
-        return self
 
-    def _init_centroids(self):
+        # Step 0
         if self.init == 'k-means++':
-            # first random centroid
-            x_pool = self.x.copy()
-            c = np.random.randint(len(x_pool))
-            self.cluster_centers_ = [x_pool[c]]
-            x_pool = np.delete(x_pool, c, axis=0)
+            x_pool = self.x.copy()  # init pool of centroids
+            c = np.random.randint(len(x_pool))  # first random index
+            self.cluster_centers_ = np.array([x_pool[c]])  # assign centroid
+            x_pool = np.delete(x_pool, c, axis=0) # remove that centroid from the pool
 
             # remaining centroids
             while len(self.cluster_centers_) < self.n_clusters:
                 # calculated distances and probabilities (max dist -> larger prob)
-                d = [self._calculate_distance(
-                    self.cluster_centers_[-1], x) for x in x_pool]
-                p = d / sum(np.power(d, 2))
-                # picking best centroid
+                d = np.sqrt(((x_pool - self.cluster_centers_[:, np.newaxis]) ** 2).sum((2, 0)).T)
+                p = d / sum(d ** 2)
+
+                # picking the best centroid from the pool
                 centroid = random.choices(range(len(p)), p)[0]
-                self.cluster_centers_ = np.append(
-                    self.cluster_centers_, [x_pool[centroid]], axis=0)
+                self.cluster_centers_ = np.append(self.cluster_centers_, [x_pool[centroid]], axis=0)
                 x_pool = np.delete(x_pool, centroid, axis=0)
 
         else:  # pick n random centroids
             self.cluster_centers_ = \
-                [self.x[i] for i in random.sample(
-                    range(len(self.x)), self.n_clusters)]
+                np.array([self.x[i] for i in random.sample(
+                    range(len(self.x)), self.n_clusters)])
 
-    def _calculate_labels(self):
-        d = [[self._calculate_distance(i, c)
-              for c in self.cluster_centers_] for i in self.x]
-        #d = self._calculate_distance(self.x, self.cluster_centers_)
-        self.labels_ = np.argmin(d, axis=1)  # indexes of min values
+        # K-Means
 
-    def _calculate_centroids(self):
-        centroids = []
-        for c in range(self.n_clusters):
-            x = [self.x[li] for li, lc in enumerate(self.labels_) if lc == c]
-            x = np.array(x).T  # make a list of one type variable values
-            centroids.append([sum(i) / len(i) for i in x])
-        self.cluster_centers_ = np.array(centroids)
+        while not np.array_equal(self.cluster_centers_old_, self.cluster_centers_) and iter < self.max_iter:
+            self.cluster_centers_old_ = self.cluster_centers_.copy()
+            print('Iteration: ' + str(iter + 1) + '; ', *self.cluster_centers_)
+            iter += 1
 
-    def _calculate_distance(self, x1, x2):  # Euclidean distance
-        return np.sqrt(sum(np.power(x1 - x2, 2)))
+            # Step 1
+            # squared distance -> sum x dimensions and centroids -> transpose (clusters in row)
+            # d = [[np.sqrt(sum(np.power(i - c, 2))) for c in self.cluster_centers_] for i in self.x]
+            d = np.sqrt(((self.x - self.cluster_centers_[:, np.newaxis]) ** 2).sum(2).T)
+            # indexes of min values (grouped by column)
+            self.labels_ = np.argmin(d, axis=1)
+
+            # Step 2
+            centroids = []
+            for c in range(self.n_clusters):
+                x = np.array([self.x[li] for li, lc in enumerate(self.labels_) if lc == c])
+                # centroids.append([sum(i) / len(i) for i in x.T])
+                centroids.append(x.mean(0)) # calculate mean for each cluster x's
+            self.cluster_centers_ = np.array(centroids)
+
+        return self
 
 
 def show_test():
-    X = pd.DataFrame({
+    x = pd.DataFrame({
         'x': [1, 2, 1, 3, 5, 4],
         'y': [5, 5, 4, 2, 2, 1]
     })
-    X['k'] = _KMeans(n_clusters=2).fit(X).labels_
-    sns.scatterplot(x=X['x'], y=X['y'], hue=X['k'])
+    x['k'] = _KMeans(n_clusters=2).fit(x).labels_
+    sns.scatterplot(x=x['x'], y=x['y'], hue=x['k'])
     plt.show()
 
 
 def show_iris():
-
-    X = pd.read_csv('iris.data.csv')
-    X['k'] = _KMeans(n_clusters=3).fit(
-        X[['sepal length', 'sepal width', 'petal length', 'petal width']]).labels_
-    sns.pairplot(X, vars=['sepal length', 'sepal width',
-                          'petal length', 'petal width'], hue='k')
+    x = pd.read_csv('datasets/iris.data.csv')
+    x['k'] = _KMeans(n_clusters=3).fit(
+        x[['sepal length', 'sepal width', 'petal length', 'petal width']]).labels_
+    
+    sns.pairplot(x, vars=['sepal length', 'sepal width', 'petal length', 'petal width'], hue='k')
     # sns.pairplot(X, vars=['sepal length', 'sepal width', 'petal length', 'petal width'], hue='class')
     plt.show()
 
+    size_of_k = x.iloc[:, [4, 5]].groupby(['class', 'k']).size()
+    max_by_class = size_of_k.groupby(['class']).max()
+
+    print('\n')
+    print(size_of_k, end='\n\n')
+    print(max_by_class, end='\n\n')
+    print('Accuracy: ', str(max_by_class.sum()), '/', len(x), '=',
+          str(round((max_by_class.sum() / len(x)), 2) * 100), '%')
+
 
 def compress_image():
-    X = imageio.imread('image.jpg')
-    k = _KMeans(n_clusters=2).fit(np.reshape(X, (-1, 3)))
-    c = list(map((lambda x: k.cluster_centers_[x].astype(int)), k.labels_))
-    plt.imshow(np.reshape(c, X.shape))
+    x = imageio.imread('datasets/image.jpg')
+    k = _KMeans(n_clusters=6).fit(np.reshape(x, (-1, 3)))
+    c = list(map((lambda y: k.cluster_centers_[y].astype(int)), k.labels_))
+    plt.imshow(np.reshape(c, x.shape))
     plt.show()
 
 
-# show_test()
-# show_iris()
-# compress_image()
+#show_test()
+#show_iris()
+compress_image()
